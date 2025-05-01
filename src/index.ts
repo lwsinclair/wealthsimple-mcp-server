85ba1eaae21c3e650f5dbe45dd9c5188d19f7958
Peter Graham <peterghm@gmail.com>
2025-05-01 13:52:51 -0400

    rerun yarn cf-typegen

--- src/index.ts
+++ src/index.ts
@@ -26,12 +26,10 @@ export default {
     const url = new URL(request.url);
 
     if (url.pathname === '/sse' || url.pathname === '/sse/message') {
-      // @ts-ignore
       return WealthsimpleMCP.serveSSE('/sse').fetch(request, env, ctx);
     }
 
     if (url.pathname === '/mcp') {
-      // @ts-ignore
       return WealthsimpleMCP.serve('/mcp').fetch(request, env, ctx);
     }
 
